5285532459a26d4a092f0fdd9bb3e181d8e18c18
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-11 01:08:28 -0800

    [add] 'inventories' to the INSTALLED_APPS array

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 'polls.apps.PollsConfig',
+    'inventories',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
