d247cba50615ba8462a777af3b4cf29df3b1a218
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-11 00:53:08 -0800

    [add] created a many to many relationships between parts and devices model

--- mysite/inventories/models.py
+++ mysite/inventories/models.py
@@ -21,6 +21,7 @@ class Part(models.Model):
     modified = models.DateTimeField(auto_now=True,
                                     help_text="The date and time this part was updated. Automatically generated when the model updates.")
     repair = models.TextField(help_text="Write the type of repaid of your part here.")
+    devices = models.ManyToManyField(Device)
 
     def __str__(self):
         return self.title
