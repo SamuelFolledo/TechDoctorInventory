014f81013c574925fde7cfb71c52689cd454abe0
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-13 07:21:55 -0800

    [wip] adding views for Devices

--- mysite/inventories/urls.py
+++ mysite/inventories/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
+from inventories.views import DeviceListView, DeviceDetailView, DeviceResultsView,# DeviceCreateView
+# from . import views #noob way
 
-from . import views
+# FUNCTION BASED VIEWS
+# urlpatterns = [
+#     path('', views.index, name='index'),
+# ]
 
+app_name = 'device_inventory'
 urlpatterns = [
-    path('', views.index, name='index'),
+    #sample for int:id and function-based views
+    # path('', views.index, name='index'), # ex: /polls/
+    # path('<int:question_id>/', views.detail, name='detail'), # ex: /polls/5/
+    # path('<int:question_id>/results/', views.results, name='results'), # ex: /polls/5/results/
+    # path('<int:question_id>/vote/', views.vote, name='vote'), # ex: /polls/5/vote/
+    
+    #Generic Views
+    path('', IndexView.as_view(), name='index'),
+    path('<str:slug>/', DeviceDetailView.as_view(), name='inventory-detail'),
+    path('<str:slug>/results/', DeviceResultsView.as_view(), name='inventory-results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('wiki/create/', DeviceCreateView.as_view(), name='wiki-create-page'), #add soon
+    path('thanks/', DeviceDetailView.as_view(), name='inventory-thanks'),
 ]
