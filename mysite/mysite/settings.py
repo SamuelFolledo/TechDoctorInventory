18d69ef7f1f423382eb509000b066e8227aa310e
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-13 16:34:19 -0800

    [add] put DJANGO's SECRET_KEY to a .env file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*wgm^oxm+*n)kff-jek#)do+^g9uk9b#oe9var296lk3@971n6'
-
+SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
