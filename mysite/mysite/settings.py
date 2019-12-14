4e27a1c1dcdda871b28543c363753b06726c29f2
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-13 16:36:39 -0800

    [add] hid or DJANGO_SECRET_KEY

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+#imports fot dotenv for DJANGO_SECRET_KEY
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_SECRET_KEY
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY") #hidden in our .env file
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
