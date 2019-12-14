ae12cfa7d71cfbacf3933217e1a0f5045454eead
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-13 16:04:17 -0800

    [add] tests for accounts app

--- mysite/accounts/tests.py
+++ mysite/accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse, reverse_lazy
 
-# Create your tests here.
+
+class SignUpViewTests(TestCase):
+    def test_true_is_true(self): #testing test
+        """ Tests if True is equal to True. Should always pass. """
+        self.assertEqual(True, True)
+
+    def test_false_is_false(self): #testing test
+        """ Tests if False is equal to False. Should always pass. """
+        self.assertEqual(False, False)
