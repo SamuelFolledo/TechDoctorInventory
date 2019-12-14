dab7b99368caee2c303f98a3087c6531251b6fab
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-13 16:14:01 -0800

    [add] optimize for device

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -3,10 +3,10 @@ from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 from inventories.models import Device
 from api.serializers import DeviceSerializer
 
-class PageList(ListCreateAPIView):
+class DeviceList(ListCreateAPIView):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
 
-class PageDetail(RetrieveDestroyAPIView):
+class DeviceDetail(RetrieveDestroyAPIView):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
